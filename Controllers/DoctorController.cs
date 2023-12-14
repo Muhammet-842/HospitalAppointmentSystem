718f73dde643828647e4a157b74c71ef524173ad
Muhammet Ali Şahal <74255054+Muhammet-842@users.noreply.github.com>
2023-12-14 04:50:24 +0300

    hasta ve doktor editleri yapıldı ama hasta kısmında hata var çözülecek

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -29,5 +29,48 @@ namespace HospitaAppointmentSystem.Controllers
             return RedirectToAction("Index");
             
         }
+        [HttpGet]
+        public async Task<IActionResult> EditDoc(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor==null)
+            {
+                return NotFound();
+            }
+            return View(doctor);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditDoc(int id, Doctor model)
+        {
+            if(id!= model.DoctorId){
+                return NotFound();
+            }
+
+            if(ModelState.IsValid){
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Doctors.Any(o=> o.DoctorId == model.DoctorId)){
+                        return NotFound();
+                    }
+                    else{
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
     }
 }
