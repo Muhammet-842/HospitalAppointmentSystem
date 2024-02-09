1eb0dfbd3574fe97671c751de496a64dc8c5af16
Muhammet Ali Şahal <74255054+Muhammet-842@users.noreply.github.com>
2024-02-09 22:59:10 +0300

    last push

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using HospitaAppointmentSystem.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
